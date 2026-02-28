deca788176332ea7937f04cf8c95c5b919e21212
SATHANA A <149176011+staynumb@users.noreply.github.com>
2026-02-28 12:31:20 +0530

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,5 @@
 # CheatShield Detection Modules
+from modules.object_detection import ObjectDetector
+from modules.face_detection import FaceDetector
+from modules.audio_detection import AudioDetector
+from modules.system_control import SystemController
